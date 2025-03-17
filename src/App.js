cce9f1da5e48635037f0ed168555f0466dc920e6
randilwelikala <randilgimantha646@gmail.com>
2025-03-17 15:00:06 +0530

    display name and age using class component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,38 @@ class App extends Component{
 
     constructor(props){
         super(props);
-    }
 
+        this.state = {
+            name:"Ruwan",
+            age:25
+        };
+
+    }
+    
+    render(){
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                    Edit 123 <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    Learn React
+                    </a>
+                    <p>{this.state.name}</p>
+                    <p>{this.state.age}</p>
+                    
+                </header>
+            </div>
+
+        )
+
+    }
 }
+export default App;
